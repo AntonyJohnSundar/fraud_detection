67c1dadbb604eb71c2458c557d52bdf1be418721
AntonyJohnSundar <antonyphilly@gmail.com>
2024-01-02 19:19:18 +0530

    mistake on merging

--- model.py
+++ model.py
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 print("Adding model.py to new branch called Model")
 
 def model1():
@@ -7,13 +6,3 @@ def model1():
 
 def model2():
     print("Model two is the best")
-=======
-print("Adding model to new branch called Model")
-
-def model1():
-    print("Modle one is the best")
-
-def model2():
-    print("Modle two is the best")
-    
->>>>>>> 590ae2a5a4bc222f299d0504732f5f605ba120ac
